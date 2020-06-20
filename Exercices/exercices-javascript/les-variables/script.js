1b256572264153f25647b362ff5c0f57c37b7579
Ezéchiel Noubissi <ezchiele@siriusschool.be>
2020-06-20 22:44:24 +0200

    Update

--- Exercices/exercices-javascript/les-variables/script.js
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -1,3 +1,20 @@
 const salutation = ' Hello World';
+console.log(salutation);
 const greet = "Bonjour je m'appelle";
 console.log(greet);
+const firstName = 'Ezéchiel';
+console.log(bonjour + ' ' + firstName);
+let age = '26 ans';
+console.log(age);
+let siriusStudentArray = [
+  'Ezéchiel',
+  'Eudes',
+  'Julie',
+  'Geremy',
+  'Aliyou',
+  'Christhophe',
+  'Lionel',
+];
+console.log(siriusStudentArray[6]);
+console.log(siriusStudentArray[0]);
+console.log(Student.name);
