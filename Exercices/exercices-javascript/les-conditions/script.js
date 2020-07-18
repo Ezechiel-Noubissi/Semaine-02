13edb88160c59b73f731d6d884acc074498395a1
Ezéchiel Noubissi <ezchiele@siriusschool.be>
2020-07-18 13:52:24 +0200

    Update

---
+++ Exercices/exercices-javascript/les-conditions/script.js
@@ -0,0 +1,22 @@
+let leftNumber = 5;
+let rightNumber = 10;
+
+const isLower = (a, b) => {
+  if (a < b) {
+    console.log(true);
+  } else {
+    console.log(false)
+  }
+}
+isLower(rightNumber, leftNumber);
+isLower(leftNumber, 10);
+
+const isDivisibleByTwo = (a) => {
+  if (a % 2 === 0) {
+    console.log(`Le chiffre ${a} est un chiffre paire`)
+  } else {
+    console.log(`Le chiffre ${a} est un chiffre impaire`)
+  }
+}
+isDivisibleByTwo(leftNumber);
+isDivisibleByTwo(rightNumber);
--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -1,5 +1,15 @@
 //Insérez ici les exercices 2.1 à 2.4
 
+const square = (a) => {
+  let result = a * a;
+  console.log(result);
+}
+
+square(2);
+square(4);
+square(8);
+square(16);
+
 // Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
 
 let firstNumber = 9;
@@ -9,18 +19,20 @@ let fourthNumber = 12;
 
 // Exercice incrémentation
 function incrementOnClick() {
-    document.getElementById("increment").addEventListener("click", function decrement() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let incrementCounter = 0;
+  document.getElementById("increment").addEventListener("click", function decrement() {
+    // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+    let incrementCounter = 0;
 
-       console.log("Incrémentez") 
-})};
+    console.log("Incrémentez")
+  })
+};
 // Exercice incrémentation
 function decrementOnClick() {
-    document.getElementById("decrement").addEventListener("click", function increment() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let decrementCounter = 100;
+  document.getElementById("decrement").addEventListener("click", function increment() {
+    // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+    let decrementCounter = 100;
 
-       console.log("Décrémentez");
-})};
+    console.log("Décrémentez");
+  })
+};
 
